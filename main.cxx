93627f8a61d9f4449e19fd80d3b552722699551a
erwin8086 <erwin.maximilian@gmail.com>
2018-11-20 17:59:53 +0100

    added fifth example

--- main.cxx
+++ main.cxx
@@ -9,7 +9,7 @@ int main()
 {
 	std::vector<struct var> *vars = new std::vector<struct var>();
 	// the exampe program
-	const char* cmd = "repeat 8 [ repeat 4 [ fd 100 rt 90 ] rt 45 ] print [repeat 20 [ make \"a :repcount * 5 + 20 repeat 8 [ repeat 4 [ fd :a rt 90 ] rt 45 ] ] ] print [fast repeat 10000 [ fd :random(-10, 10) rt 90 * :int( :random(5) ) ] slow] print [make \"a :int( :random(3, 16) ) make \"b :int( :random(3, 16) ) repeat :a [ repeat :b [ fd 400 / :b rt 360 / :b ] rt 360 / :a ] ] print [fast repeat 80 [ make \"i :repcount repeat 35 [ fd :i rt 10 ] ] slow ]";
+	const char* cmd = "repeat 8 [ repeat 4 [ fd 100 rt 90 ] rt 45 ] print [repeat 20 [ make \"a :repcount * 5 + 20 repeat 8 [ repeat 4 [ fd :a rt 90 ] rt 45 ] ] ] print [fast repeat 10000 [ fd :random(-10, 10) rt 90 * :int( :random(5) ) ] slow] print [make \"a :int( :random(3, 16) ) make \"b :int( :random(3, 16) ) repeat :a [ repeat :b [ fd 400 / :b rt 360 / :b ] rt 360 / :a ] ] print [fast repeat 80 [ make \"i :repcount repeat 35 [ fd :i rt 10 ] ] slow ] print [repeat 36 [ make \"len :repcount * 4 repeat 2 [ fd :len rt 90 fd :len / 2 rt 90 ] rt 10 ] ]";
 	LogoGUI *g = new LogoGUI(400, 500);
 	Parser *p = new Parser(cmd, vars, true);
 	p->execute(g);
